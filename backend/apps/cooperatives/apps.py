14866687076e37a591237da7136deb091d385059
Your Name <manojkc1dev@gmail.com>
2026-08-17 21:38:53 +0545

    Fresh start: clearing previous project files

--- backend/apps/cooperatives/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class CooperativesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.cooperatives'
--- backend/config/settings/production.py
+++
@@ -1,7 +0,0 @@
-from .base import *
-
-# Explicitly override DEBUG to False for production safety
-DEBUG = False
-
-# Add production domains here
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='example.com').split(',')
